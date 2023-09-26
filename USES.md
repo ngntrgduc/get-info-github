f2cee6117a5e373742b6b5c951fc18609e6b8a10
ngntrgduc <trungducnguyen1511@gmail.com>
2023-09-26 18:46:27 +0700

    Change to OOP style, complete starred functionality, refactor code

--- USES.md
+++ USES.md
@@ -1,8 +1,6 @@
 Here's my crawled repositories list, using GitHub API. 
 
-I find myself being lost when visiting someone's repositories. If that user has more than 100 repositories, then it will be very exhausted...
-
-And here be dragons 🐉.
+I often find myself being lost when visiting someone's repositories. If that user has more than 100 repositories, then it will be very exhausted... So I made this to crawl repositories of GitHub users painlessly.
 
 **PS**: I made this to view repositories of other accounts. But then I realized this can be used for personal.
 
@@ -18,8 +16,11 @@ And here be dragons 🐉.
     ```
 - Run `main.py`.
 
+If you want to ignore specific repositories: Create a `ignore.txt` file, and write the name of the repositories you want to ignore, separated by line.
+
 For more information, visit: https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-a-user
 
 ### TODO
 - [x] `Ignore` repo lists
-- [ ] (*WIP*) Starred repo crawler
+- [x] Crawl all repositories (for users with 100+ repositories)
+- [x] Starred repo crawler
--- main.py
+++ main.py
@@ -1,49 +1,109 @@
-# import json
+import json
 import requests
 from dotenv import dotenv_values
 
-TOKEN = dotenv_values('.env')['GITHUB_TOKEN']
-username = 'ngntrgduc'
-url = f"https://api.github.com/users/{username}/repos"
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": "Bearer " + TOKEN,
-    "X-GitHub-Api-Version": "2022-11-28",
-}
-params = {
-    "sort": "pushed",
-    "per_page": 100,
-    "page": 1,
-}
-
-data = requests.get(url, headers=headers, params=params).json()
-
-# Backup data
-# with open('result.json', 'w', encoding='utf-8') as f:
-#     json.dump(result, f, indent=2)
-# data = json.load(open('result.json', 'r', encoding='utf-8'))
-
-# Ignore specific repo
-with open('ignore.txt', 'r', encoding='utf-8') as f:
-    ignore_repo = f.read().splitlines()
-
-with open('README.md', 'w', encoding='utf-8') as f:
-    f.write('| **Repository** | **Description** |\n')
-    f.write('| -------------- | --------------- |\n')
-
-    for i in range(len(data)):
-        repo_name   = data[i]['name']
-        repo_url    = data[i]['html_url']
-        description = data[i]['description']
-        fork        = data[i]['fork']
-        # stars       = data[i]['stargazers_count']
-        # language    = data[i]['language']
-
-        fork = '(*fork*)' if fork else ''
-        if not description:
-            description = ''
+class User:
+    def __init__(self, username: str) -> None:
+        self.username = username
+        self.url = f'https://api.github.com/users/{self.username}/repos'
+        TOKEN = dotenv_values('.env')['GITHUB_TOKEN']
+        self.headers = {
+            "Accept": "application/vnd.github+json",
+            "Authorization": "Bearer " + TOKEN,
+            "X-GitHub-Api-Version": "2022-11-28",
+        }
+        self.params = {
+            "sort": "pushed",
+            "per_page": 100,
+            "page": 1,
+        }
+    
+    def get_data(self, all: bool = False) -> None:
+        """Get data from GitHub API"""
         
-        if repo_name in ignore_repo:
-            continue
+        if not all:
+            self.data = requests.get(self.url, headers=self.headers, params=self.params).json()
+        else:
+            self.data = []
+            while True:
+                data = requests.get(self.url, headers=self.headers, params=self.params).json()
+                if not data:
+                    break
+                self.params['page'] += 1
+                self.data += data
+    
+    def backup(self, file_name: str = 'result.json') -> None:
+        """Write a backup to file"""
 
-        f.write(f'| **[{repo_name}]({repo_url})** {fork} | {description} |\n')
+        with open(file_name, 'w', encoding='utf-8') as f:
+            json.dump(self.data, f, indent=2)
+    
+    def get_ignore(self, file_name: str = 'ignore.txt'):
+        """Get ignore repo from file"""
+
+        with open(file_name, 'r', encoding='utf-8') as f:
+            ignore_repo = f.read().splitlines()
+
+        return ignore_repo
+    
+    def get_repositories(self, file_name: str = 'README.md', ignore: bool = True, all: bool = False) -> None:
+        """Get repositories of user"""
+
+        self.get_data(all)
+        
+        if ignore:
+            ignore_repo = self.get_ignore()
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('| **Repository** | **Description** |\n')
+            f.write('| -------------- | --------------- |\n')
+
+            for i in range(len(self.data)):
+                repo_name   = self.data[i]['name']
+                repo_url    = self.data[i]['html_url']
+                description = self.data[i]['description']
+                fork        = self.data[i]['fork']
+                # stars       = self.data[i]['stargazers_count']
+                # language    = self.data[i]['language']
+
+                fork = '(*fork*)' if fork else ''
+                if not description:
+                    description = ''
+                
+                if repo_name in ignore_repo:
+                    continue
+
+                f.write(f'| **[{repo_name}]({repo_url})** {fork} | {description} |\n')
+
+    def get_starred(self, file_name: str = 'STARRED.md', all: bool = False) -> None:
+        """Get starred repositories of user"""
+
+        self.url = f'https://api.github.com/users/{self.username}/starred'
+        self.get_data(all)
+
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write('### My starred repositories\n')
+            f.write('| **Repository** | **Stars** | **Language** | **Description** |\n')
+            f.write('| -------------- | --------- | ------------ | --------------- |\n')
+
+            for i in range(len(self.data)):
+                repo_name   = self.data[i]['full_name']
+                repo_url    = self.data[i]['html_url']
+                language    = self.data[i]['language']
+                description = self.data[i]['description']
+                stars       = self.data[i]['stargazers_count']
+
+                if not description:
+                    description = ''
+
+                f.write(f'| **[{repo_name}]({repo_url})** | **{stars}** | *{language}* | {description}\n')
+        pass
+
+def main():
+    user = User('ngntrgduc')
+    user.get_repositories()
+    user.get_starred()
+    pass
+
+if __name__ == '__main__':
+    main()
